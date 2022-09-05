3604b58e3a82eacfd7a0ea3c994cb5aaaef16e02
William James <Williamj3795@gmail.com>
2022-09-05 15:27:58 -0400

    updated html,css, added variables for document.queryselector and questions

--- Assets/script.js
+++ Assets/script.js
@@ -1,14 +1,23 @@
 // elements linked by ID //
 var startButton = document.querySelector("#startBtn");
-var timerEl = document.querySelector('#timer');
+var timer = document.querySelector('#timer');
 
-var b1El = document.querySelector('#b1');
-var b2El = document.querySelector('#b2');
-var b3El = document.querySelector('#b3');
-var b4El = document.querySelector('#b4');
+var b1 = document.querySelector('#b1');
+var b2 = document.querySelector('#b2');
+var b3 = document.querySelector('#b3');
+var b4 = document.querySelector('#b4');
 
-var questionEl = document.querySelector('#question');
+var question = document.querySelector('#question');
 
-var openingEl = document.querySelector("#opening");
-var quizEl = document.querySelector("#quiz");
+var opening = document.querySelector("#opening");
+var quiz = document.querySelector("#quiz");
 
+var questions = [
+	{
+		question: "What bulit-in method calls a function for each element in the array?",
+		q1: "while()",
+		q2: "loop()",
+		q3: "forEach()",
+		q4: "None of the above",
+		answer: "forEach()"
+	},
