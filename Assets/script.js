faf52a796af04918568eb900b907fc7df80a535f
William James <Williamj3795@gmail.com>
2022-09-05 14:59:12 -0400

    updated html,css,js

--- Assets/script.js
+++ Assets/script.js
@@ -2,3 +2,13 @@
 var startButton = document.querySelector("#startBtn");
 var timerEl = document.querySelector('#timer');
 
+var b1El = document.querySelector('#b1');
+var b2El = document.querySelector('#b2');
+var b3El = document.querySelector('#b3');
+var b4El = document.querySelector('#b4');
+
+var questionEl = document.querySelector('#question');
+
+var openingEl = document.querySelector("#opening");
+var quizEl = document.querySelector("#quiz");
+
